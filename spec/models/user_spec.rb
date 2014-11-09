e403390546825b8ec5f869ce3a4b24586e94b015
sqrtxx <sqrtxx@gmail.com>
2014-11-10 00:09:02 +0900

    Add .recent method in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { create :person }
+
+  describe '.recent' do
+    let(:user_1_day_ago) { create :person, created_at: 1.day.ago }
+    let(:user_2_days_ago) { create :person, created_at: 2.days.ago }
+    before(:each) { user_1_day_ago; user_2_days_ago }
+    it { expect(User.recent).to eq [user_1_day_ago, user_2_days_ago] }
+  end
+
 end
