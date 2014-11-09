a98af88f6ada4e398c11581babff1431aed2fb3d
sqrtxx <sqrtxx@gmail.com>
2014-11-10 01:37:56 +0900

    Display 10 users in index

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,4 +1,5 @@
 class MainPageController < ApplicationController
   def index
+    @users = User.recent.limit(10)
   end
 end
--- spec/controllers/main_page_controller_spec.rb
+++ spec/controllers/main_page_controller_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe MainPageController do
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get :index
-      response.should be_success
-    end
+    before(:each) { create_list(:person, 20) }
+    subject { get :index }
+    it { expect(subject.status).to eq 200 }
+    it { subject; expect(assigns(:users).length).to eq 10 }
+    it { expect(subject).to render_template 'main_page/index' }
   end
 
 end
