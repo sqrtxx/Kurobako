edff1ba82572cf2733480e06f2ce28733ae0e131
sqrtxx <sqrtxx@gmail.com>
2014-11-10 15:52:52 +0900

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  validates :screen_name, presence: true, length: { in: 3..20 }
+  validates :full_name, length: { maximum: 255 }
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :description, length: { maximum: 255 }
+  validates :url, format: URI::regexp(%w(http https))
+
   scope :recent, -> { order('created_at DESC') }
 end
