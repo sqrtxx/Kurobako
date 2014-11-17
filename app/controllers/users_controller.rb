18af669b053cc1b34d63d416f4491835499d1aa4
sqrtxx <sqrtxx@gmail.com>
2014-11-17 22:54:45 +0900

    Fix user params permition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:screen_name, :full_name, :email, :description, :url)
+    params.require(:user).permit(:screen_name, :full_name, :email, :description, :url, :password, :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
   validates :full_name, length: { maximum: 255 }
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
   validates :description, length: { maximum: 255 }
-  validates :url, format: URI::regexp(%w(http https))
   validates :password, length: { in: 7..20 }, format: { with: /(^[0-9a-zA-Z]+$)/ }
 
   scope :recent, -> { order('created_at DESC') }
