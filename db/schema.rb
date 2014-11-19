3faee1d5a38ad1d9e00674ffadb2675eb24b1a69
sqrtxx <sqrtxx@gmail.com>
2014-11-19 17:11:17 +0900

    Add remember_token column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117122908) do
+ActiveRecord::Schema.define(version: 20141119080517) do
 
   create_table "users", force: true do |t|
     t.string   "screen_name"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20141117122908) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
