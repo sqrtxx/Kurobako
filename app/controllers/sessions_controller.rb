4c2a73ab1fd314feacfc7a6d619a9a2ae3d82a27
sqrtxx <sqrtxx@gmail.com>
2014-11-19 17:57:52 +0900

    Add session methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
+      sign_in user
       redirect_to root_url
     end
   end
 
   def destroy
+    sign_out
+    redirect_to root_url
   end
 
 end
