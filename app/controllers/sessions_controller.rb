a7d2b0a3897af9fe975409d22decd53f635dfa84
sqrtxx <sqrtxx@gmail.com>
2014-11-19 17:03:21 +0900

    Add sessions new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,12 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      redirect_to user
+      redirect_to root_url
     end
   end
 
